beb5f05b47fbfcd6d340fba16ddc2d2ac0add3f2
chirayu-joshi <chirayujoshi1189@gmail.com>
2021-03-12 19:08:52 +0530

    Added CustomBoxShadow Widget

--- crypto_vault/lib/widgets/drawer_item.dart
+++ crypto_vault/lib/widgets/drawer_item.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:crypto_vault/constants.dart';
+import 'package:crypto_vault/widgets/custom_box_shadow.dart';
 import 'package:crypto_vault/providers/screens.dart';
 
 class DrawerItem extends StatelessWidget {
@@ -27,10 +28,21 @@ class DrawerItem extends StatelessWidget {
       child: Container(
         width: double.infinity,
         padding: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+        margin: EdgeInsets.symmetric(vertical: 2),
         decoration: BoxDecoration(
           color: screensData.screenName == title
-              ? Colors.white30
+              ? Colors.white24
               : Colors.transparent,
+          boxShadow: screensData.screenName == title
+              ? [
+                  CustomBoxShadow(
+                    color: Color(0x10000000),
+                    blurRadius: 16,
+                    offset: Offset.zero,
+                    blurStyle: BlurStyle.outer,
+                  ),
+                ]
+              : [],
           borderRadius: BorderRadius.circular(12),
         ),
         child: Row(
